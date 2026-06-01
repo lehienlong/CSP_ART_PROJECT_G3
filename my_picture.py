6d8c77eb0ef750d47b2b95b5a132201b54cc330e
dnguyen28-afk <dnguyen28@ssis.edu.vn>
2026-06-01 08:09:30 +0700

    Background With Clouds

--- my_picture.py
+++ my_picture.py
@@ -2,46 +2,15 @@ import simple_graphics as sg
 
 def draw_picture(width, height):
     """Draws a static picture."""
-    
-    # Fill the background
-    sg.fill_background("white")
-    
-    # make some variables available
-    colors = ["red", "green", "blue", "cyan", "magenta", "yellow"]
-    
-    triangle_height = height/5
-    triangle_width = width / 3
-    
-    # Draw the tesselation
-    # code for red triangles
-    sg.set_fill_color(colors[0])
-    
-    # call fill
-    sg.fill_circle(450, 50,50)
+    sg.set_fill_color("sky blue")
+    sg.fill_rectangle(0, 0, width, height)
+    sg.set_fill_color("white")
+    sg.draw_cloud(100, 50, 40)
+    sg.set_fill_color("white")
+    sg.draw_cloud(300, 75, 40)
+    sg.set_fill_color("white")
+    sg.draw_cloud(500, 50, 40)
 
-    # draw a mountain
-    sg.set_fill_color("#827e7e") # relatively dark gray
-    sg. fill_triangle(300, 150, 400, 20, 350, 150)
-    sg.set_fill_color("#c7c1c1") # lighter gray
-    sg. fill_triangle(350, 150, 400, 20, 550, 150)
-
-    # draw horizon
-    sg.set_outline_color("black")
-    sg.set_line_thickness(1)
-    sg.draw_line(0, 150, 600, 150
-    
-    # Define the points the curve should bend through
-    river_points = [
-        (100, 150), # Start point
-        (300, 200), # Bends towards here
-        (200, 350), # Bends back here
-        (500, 500)  # End point
-    ]
-
-    sg.set_outline_color("blue")
-    sg.set_line_thickness(8)
-    sg.draw_curve(river_points)
-    
 
 if __name__ == "__main__":
     # Launch the wrapper; only edit starting dimensions of canvas if you would like to
